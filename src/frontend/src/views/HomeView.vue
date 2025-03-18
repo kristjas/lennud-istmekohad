12532f1db14b59db75aadf34507ffb7e68067682
Kristjan Säärits <kristjan.saarits@ut.ee>
2025-03-18 21:10:31 +0200

    Fetching the flights to the frontend

--- src/frontend/src/views/HomeView.vue
+++ src/frontend/src/views/HomeView.vue
@@ -1,14 +1,23 @@
 <template>
   <div>
-    
+    <h2>Available Flights</h2>
+    <ul>
+      <li v-for="flight in flights" :key="flight.id">
+        <strong>{{ flight.sihtkoht }}</strong> ({{ flight.kuupäev }})
+        - Starts at: {{ flight.lennualgusaeg }} | Ends at: {{ flight.lennulõppaeg }} 
+        - Price: €{{ flight.hind }} | Aircraft: {{ flight.lennuk }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script setup>
 import { ref, onMounted } from "vue";
-
+import { getFlights } from "@/api";
 
 const flights = ref([]);
 
-
+onMounted(async () => {
+  flights.value = await getFlights();
+});
 </script>
